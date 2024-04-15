6ce926b69cc96cff8a4a15927fa3c946b93a5ea3
Dev <devsapariya94@gmail.com>
2024-04-15 15:57:43 +0530

    update requirement

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import streamlit as st
 from llama_index.llms.gemini import Gemini
 from llama_index.embeddings.gemini import GeminiEmbedding
 from llama_index.core.readers import SimpleDirectoryReader
