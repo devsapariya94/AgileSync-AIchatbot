9b7a8e8b9a1dca5f3da990be92a069112c70e879
Dev <devsapariya94@gmail.com>
2024-04-15 16:20:19 +0530

    testing

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import streamlit as st
-# from llama_index.llms.gemini import Gemini
-# from llama_index.embeddings.gemini import GeminiEmbedding
-# from llama_index.core.readers import SimpleDirectoryReader
-# from llama_index.core import VectorStoreIndex
-# from llama_index.core import ServiceContext
+from llama_index.llms.gemini import Gemini
+from llama_index.embeddings.gemini import GeminiEmbedding
+from llama_index.core.readers import SimpleDirectoryReader
+from llama_index.core import VectorStoreIndex
+from llama_index.core import ServiceContext
 
 
 
-# from langchain_google_genai import ChatGoogleGenerativeAI
-# from langchain.chains import ConversationChain
-# from langchain.memory import ConversationBufferMemory
-# from langchain_core.prompts import ChatPromptTemplate  # Fix import
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+from langchain_core.prompts import ChatPromptTemplate  # Fix import
 
 
-# from pymongo import MongoClient
+from pymongo import MongoClient
 
-# import os
-# import requests
-# import shutil
+import os
+import requests
+import shutil
 
 st.title("Hello World")
